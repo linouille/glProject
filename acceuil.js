f989f8f7bd7e3d861d444a972e98acc039453e22
lina <linaboumghar0@gmail.com>
2023-12-26 00:06:10 +0100

    new update

---
+++ acceuil.js
@@ -0,0 +1,11 @@
+function changepage() {
+    window.location.href = "rdv.html";
+}
+
+function changepageB() {
+    window.location.href = "his.html";
+}
+
+function changepageC() {
+    window.location.href = "fb.html";
+}
